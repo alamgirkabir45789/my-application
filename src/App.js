5fd81f4e653f3970a6595aad99adb46ed6dad371
Alamgir <alamgirkabir45789@gmail.com>
2022-06-02 11:43:58 +0600

    also app change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2 style={{ textAlign: "center", backgroundColor: "gray" }}>
+      <h2 style={{ textAlign: "center", backgroundColor: "blue" }}>
         Hello Bangladesh
       </h2>
     </div>
