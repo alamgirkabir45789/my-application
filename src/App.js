bd3f16cc998978d46e48d23d9a3c4833b10a997c
Alamgir <alamgirkabir45789@gmail.com>
2022-06-02 16:25:24 +0600

    modify app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
+import { Card, Col, FormGroup, Input, Label, Row } from "reactstrap";
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <h2>Hello Bangladesh</h2>
+      <div>
+        <h1>Test Form</h1>
+      </div>
+      <Card className="p-3 m-5">
+        <Row>
+          <Col sm={12} xs={12} md={4} lg={4} xl={4}>
+            <FormGroup className="d-flex">
+              <Label sm="3" size="sm" for="input-small-horizontal">
+                Email
+              </Label>
+              <span style={{ marginRight: "10px", marginLeft: "10px" }}>:</span>
+              <Input
+                type="text"
+                id="input-small-horizontal"
+                bsSize="sm"
+                placeholder="Small Input"
+              />
+            </FormGroup>
+          </Col>
+          <Col sm={12} xs={12} md={4} lg={4} xl={4}>
+            <FormGroup className="d-flex">
+              <Label sm="3" size="sm" for="input-small-horizontal">
+                Phone
+              </Label>
+              <span style={{ marginRight: "10px", marginLeft: "10px" }}>:</span>
+              <Input
+                type="text"
+                id="input-small-horizontal"
+                bsSize="sm"
+                placeholder="Small Input"
+              />
+            </FormGroup>
+          </Col>
+          <Col sm={12} xs={12} md={4} lg={4} xl={4}>
+            <FormGroup className="d-flex">
+              <Label sm="3" size="sm" for="input-small-horizontal">
+                Address
+              </Label>
+              <span style={{ marginRight: "10px", marginLeft: "10px" }}>:</span>
+              <Input
+                type="text"
+                id="input-small-horizontal"
+                bsSize="sm"
+                placeholder="Small Input"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 }
