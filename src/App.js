790d880548d98dbd432315fb916634ad09bd9c56
Alamgir <alamgirkabir45789@gmail.com>
2022-06-02 11:45:31 +0600

    my app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2 style={{ textAlign: "center", backgroundColor: "blue" }}>
-        Hello Bangladesh
-      </h2>
+      <h2>Hello Bangladesh</h2>
     </div>
   );
 }
